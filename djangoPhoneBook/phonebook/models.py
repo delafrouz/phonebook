2a9379f22ef1c25f61d07069eefd62d1a7763704
Delafrouz <del.mirfendereski@gmail.com>
2023-02-06 18:11:33 +0330

    Add models

--- djangoPhoneBook/phonebook/models.py
+++ djangoPhoneBook/phonebook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    user_id = models.IntegerField(primary_key=True)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    id = models.IntegerField(primary_key=True)
+    phone_number = models.CharField(max_length=20)
+    email = models.CharField(max_length=80)
+    address = models.CharField(max_length=80)
+    secondary_phone_number = models.CharField(max_length=80)
+    job = models.CharField(max_length=20)
